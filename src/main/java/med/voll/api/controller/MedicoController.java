6a10308bcd53cb8db3ebc02e0be2bd673375980f
leidianeAguiar <leiidiane.aguiar@gmail.com>
2024-08-28 20:47:08 -0300

    Adiciona paginacão na listagem de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,9 @@ import med.voll.api.domain.dto.DadosCadastroMedicoDTO;
 import med.voll.api.domain.entity.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -33,7 +34,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedicoDTO> listar() {
-        return repository.findAll().stream().map(DadosListagemMedicoDTO::new).toList();
+    public Page<DadosListagemMedicoDTO> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedicoDTO::new);
     }
 }
